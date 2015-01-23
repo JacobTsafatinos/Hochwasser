23480f60143a93e9a8b2f2e8f83d987ca85466e5
Michael <Michael@Michaels-Infinite-Abyss-of-Time.local>
2015-01-23 00:41:36 -0500

    Remove map controls, add pin

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,10 +1,23 @@
 function initialize() {
   var mapCanvas = document.getElementById('map-canvas');
+  var myLatlng = new google.maps.LatLng(43.665771, -79.397440);
   var mapOptions = {
-    center: new google.maps.LatLng(44.5403, -78.5463),
-    zoom: 8,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
+    center: myLatlng,
+    zoom: 15,
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    disableDefaultUI: true,
+    draggable: false,
+    disableDoubleClickZoom: false,
+    mapTypeControl: false,
+    scrollwheel: false,
+    streetViewControl: false
   }
   var map = new google.maps.Map(mapCanvas, mapOptions);
+
+  var marker = new google.maps.Marker({
+    position: myLatlng,
+    map: map,
+    title: 'Theater'
+  });
 }
 google.maps.event.addDomListener(window, 'load', initialize);
