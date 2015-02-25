a2e54d6d0cb6561701132803844a447fcf0765fd
Michael <michael.kozakov@gmail.com>
2015-02-24 19:56:26 -0500

    Add reservations button

---
+++ public/javascripts/main.js
@@ -0,0 +1,5 @@
+function newPopup() {
+  window.location.href = "http://www.eventbrite.com/e/hochwasser-iii-uoft-german-production-tickets-10192378679";
+}
+
+document.getElementById("reservationBtn").addEventListener("click", newPopup);
