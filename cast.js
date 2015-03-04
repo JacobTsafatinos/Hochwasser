553e401c5f072f0634850b80ef10648d60edc301
Michael <michael.kozakov@gmail.com>
2015-03-03 23:49:10 -0500

    Move cast-crew info into separate file

---
+++ cast.js
@@ -0,0 +1,127 @@
+module.exports = [
+  {
+    name: "Erol Boran",
+    role: "Direktor",
+    image: "erol.jpg",
+    description: "this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text"
+  }, 
+
+  {
+    name: "Sonja Mertz",
+    role: "Produkzions Manager",
+    image: "sonja.jpg",
+    description: "this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text"
+  },
+
+  {
+    name: "Ana Maytah",
+    role: "Produkzions Manager",
+    image: "ana.jpg",
+    description: "this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text"
+  },
+
+  {
+    name: "Tamara Lipowski",
+    role: "Perle",
+    image: "tamara.jpg",
+    description: "this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text"
+  },
+
+  {
+    name: "Karen Fuhrmann",
+    role: "Strich",
+    image: "karen.jpg",
+    description: "this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text"
+  },
+
+  {
+    name: "Pawel Stech",
+    role: "Leo",
+    image: "pawel.jpg",
+    description: "this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text"
+  },
+
+  {
+    name: "Frances Maranger",
+    role: "Jutta",
+    image: "frances.jpg",
+    description: "this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text"
+  },
+
+  {
+    name: "Michael Kozakov",
+    role: "Kongo",
+    image: "michael.jpg",
+    description: "this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text"
+  },
+
+  {
+    name: "Jacob Tsafatinos",
+    role: "Noah",
+    image: "jacob.jpg",
+    description: "this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text"
+  },
+
+  {
+    name: "Emily Chiang",
+    role: "Betty",
+    image: "emily.jpg",
+    description: "this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text"
+  },
+
+  {
+    name: "Benjamin Tang",
+    role: "Henn",
+    image: "ben.jpg",
+    description: "this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text"
+  },
+
+  {
+    name: "Aneta Latoszewski",
+    role: "Taube",
+    image: "aneta.jpg",
+    description: "this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text"
+  },
+
+  {
+    name: "Darcie Pei",
+    role: "Praline",
+    image: "darcie.jpg",
+    description: "this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text"
+  },
+
+  {
+    name: "Alekzia Hosein",
+    role: "Butterblume",
+    image: "alekzia.jpg",
+    description: "this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text"
+  },
+
+  {
+    name: "Sunny Jang",
+    role: "Glocke",
+    image: "sunny.jpg",
+    description: "this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text"
+  },
+
+  {
+    name: "Julia Norkus",
+    role: "Brause",
+    image: "julia.jpg",
+    description: "this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text"
+  },
+
+  {
+    name: "Zoe Plessas",
+    role: "?",
+    image: "zoie.jpg",
+    description: "this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text"
+  },
+
+  {
+    name: "Maria Sokulsky-Dolnycky",
+    role: "Frl. Sommer",
+    image: "maria.jpg",
+    description: "this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text. this is placeholder text"
+  }
+]
